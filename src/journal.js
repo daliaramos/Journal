9bed2f329dde73501f947ab8d656244c281fe763
dalia ramos and Emily Watkins <dalia@>
2018-03-12 11:30:26 -0700

    add getTeaser function.

--- src/journal.js
+++ src/journal.js
@@ -12,8 +12,19 @@ Entry.prototype.LetterCount = function(str)
   var v = str.match(/[aeiou]/gi);
   var c = str.match(/[bcdfghjklmnpqrstvxzwy]/gi);
 
-  // var consonants = count(/[bcdfghjklmnpqrstvxzwy]/ig);
   var vowels = v === null ? 0 : v.length;
   var consonants = c === null ? 0 : c.length;
+
   return [vowels, consonants];
+};
+
+Entry.prototype.getTeaser = function(str) {
+  var sentence = str.split('.');
+  var wordSplit = sentence[0].split(' ');
+  if (wordSplit.length <= 8) {
+    return sentence[0];
+  } else {
+    return str.split(/\s+/).slice(0,8).join(" ");
+  }
+
 };
--- src/main.js
+++ src/main.js
@@ -10,6 +10,6 @@ $(document).ready(function() {
     $('#solution').append("<li> Word count: " + new_entry.WordCount(new_entry.body) + "</li>");
     $('#solution').append("<li> Vowel count: " + new_entry.LetterCount(new_entry.body)[0] + "</li>");
     $('#solution').append("<li> Consonant count: " + new_entry.LetterCount(new_entry.body)[1] + "</li>");
-
+    $('#solution').append("<li> Teaser: " + new_entry.getTeaser(new_entry.body) + "</li>");
   });
 });
